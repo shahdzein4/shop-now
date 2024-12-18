9455253aa13944448658d609b3ac2f86cff7bf54
shahd <147739362+shahdzein4@users.noreply.github.com>
2024-12-19 00:55:35 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+
+if(bar){
+  bar.addEventListener('click', () => {
+    navbar.classList.add('active');
+  })
+}
+
+if(close){
+  close.addEventListener('click', () => {
+    navbar.classList.remove('active');
+  })
+}
